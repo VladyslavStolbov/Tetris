12acbb5cf3763dd199ca29b1f2514d10a23dcb47
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-08-02 15:16:04 +0300

    Implement spawning

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,10 +7,15 @@ using Random = UnityEngine.Random;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
-
+    public Vector3Int spawnPosition;
+    
     private void Awake()
     {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+        
         for (int i = 0; i < this.tetrominoes.Length; i++)
         {
             this.tetrominoes[i].Initialize();
@@ -25,6 +30,18 @@ public class Board : MonoBehaviour
     public void SpawnPiece()
     {
         int random = Random.Range(0, this.tetrominoes.Length);
-        TetrominoData date = this.tetrominoes[random];
+        TetrominoData data = this.tetrominoes[random];
+        
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 }
