03c246c0ba524dfdbe144ef96c0c049b80328cd1
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-09-03 05:47:41 +0300

    Rearrange methods

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,28 +7,28 @@ public class Board : MonoBehaviour
     public Tilemap tilemap { get; private set; }
     public Piece activePiece { get; private set; }
     public Piece nextPiece { get; private set; }
-    
+
     public TetrominoData[] tetrominoes;
-    public Vector3Int spawnPosition = new (0, 8, 0);
-    public Vector3Int previewPosition = new (10, 5, 0);
-    public Vector2Int boardSize = new (10, 20);
+    public Vector3Int spawnPosition = new(0, 8, 0);
+    public Vector3Int previewPosition = new(10, 5, 0);
+    public Vector2Int boardSize = new(10, 20);
 
     public RectInt Bounds
     {
         get
         {
-            Vector2Int position = new (-boardSize.x / 2, -boardSize.y / 2);
+            Vector2Int position = new(-boardSize.x / 2, -boardSize.y / 2);
             return new RectInt(position, boardSize);
         }
     }
-    
+
     private void Awake()
     {
         tilemap = GetComponentInChildren<Tilemap>();
         activePiece = GetComponentInChildren<Piece>();
         nextPiece = gameObject.AddComponent<Piece>();
         nextPiece.enabled = false;
-        
+
         for (int i = 0; i < tetrominoes.Length; i++)
         {
             tetrominoes[i].Initialize();
@@ -39,6 +39,20 @@ public class Board : MonoBehaviour
     {
         SetNextPiece();
         SpawnPiece();
+    }
+
+    private void SetNextPiece()
+    {
+        if (nextPiece.cells != null)
+        {
+            Clear(nextPiece);
+        }
+
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[random];
+
+        nextPiece.Initialize(this, previewPosition, data);
+        Set(nextPiece);
     }
     
     public void SpawnPiece()
@@ -53,29 +67,15 @@ public class Board : MonoBehaviour
         {
             GameOver();
         }
-        
-        SetNextPiece();
-    }
-    
-    private void SetNextPiece()
-    {
-        if (nextPiece.cells != null)
-        {
-            Clear(nextPiece);
-        }
 
-        int random = Random.Range(0, tetrominoes.Length);
-        TetrominoData data = tetrominoes[random];
-        
-        nextPiece.Initialize(this, previewPosition, data);
-        Set(nextPiece);
+        SetNextPiece();
     }
 
     private void GameOver()
     {
         tilemap.ClearAllTiles();
     }
-    
+
     public void Set(Piece piece)
     {
         for (int i = 0; i < piece.cells.Length; i++)
@@ -84,7 +84,7 @@ public class Board : MonoBehaviour
             tilemap.SetTile(tilePosition, piece.data.tile);
         }
     }
-    
+
     public void Clear(Piece piece)
     {
         for (int i = 0; i < piece.cells.Length; i++)
@@ -106,14 +106,13 @@ public class Board : MonoBehaviour
             {
                 return false;
             }
-            
+
             if (!bounds.Contains((Vector2Int)tilePosition))
             {
                 return false;
             }
-
         }
-        return true;            
+        return true;
     }
 
     public void ClearLines()
@@ -140,7 +139,7 @@ public class Board : MonoBehaviour
 
         for (int col = bounds.xMin; col < bounds.xMax; col++)
         {
-            Vector3Int position = new (col, row, 0);
+            Vector3Int position = new(col, row, 0);
 
             if (!tilemap.HasTile(position))
             {
@@ -157,19 +156,19 @@ public class Board : MonoBehaviour
 
         for (int col = bounds.xMin; col < bounds.xMax; col++)
         {
-            Vector3Int position = new (col, row, 0);
-            tilemap.SetTile(position, null); 
+            Vector3Int position = new(col, row, 0);
+            tilemap.SetTile(position, null);
         }
 
         while (row < bounds.yMax)
         {
             for (int col = bounds.xMin; col < bounds.xMax; col++)
             {
-                Vector3Int position = new (col, row + 1, 0);
+                Vector3Int position = new(col, row + 1, 0);
                 TileBase above = tilemap.GetTile(position);
 
                 position = new Vector3Int(col, row, 0);
-                tilemap.SetTile(position, above);  
+                tilemap.SetTile(position, above);
             }
 
             row++;
