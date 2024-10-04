95eff41c48f9731733818730ceef8b5ccfb67bb9
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-10-04 07:05:08 +0300

    work on sound manager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,28 +3,55 @@ using UnityEngine;
 
 public enum SoundType
 {
-	FX,
-	Music
+	Click,
+	Drop,
+	GameOver,
+	LineClear,
+	Rotation,
+	TetrisClear
 }
 
-[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
 public class SoundManager : MonoBehaviour
 {
-	private static SoundManager instance;
-	private AudioSource audioSource;
+	[SerializeField] private SoundList[] soundList;
+	private static SoundManager _instance;
+	private AudioSource _audioSource;
 
 	private void Awake()
 	{
-		instance = this;
+		_instance = this;
 	}
 
 	private void Start()
 	{
-		audioSource = GetComponent<AudioSource>();
+		_audioSource = GetComponent<AudioSource>();
 	}
 
 	public static void PlaySound(SoundType sound, float volume = 1)
 	{
-		
+		AudioClip[] clips = _instance.soundList[(int)sound].Sounds;
+		AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+		_instance._audioSource.PlayOneShot(randomClip, volume);
 	}
+	
+#if UNITY_EDITOR
+	private void OnEnable()
+	{
+		string[] names = Enum.GetNames(typeof(SoundType));
+		Array.Resize(ref soundList, names.Length);
+		for (int i = 0; i < soundList.Length; i++)
+		{
+			soundList[i].name = names[i];
+		}
+	}
+#endif
+}
+
+[Serializable]
+public struct SoundList
+{
+	public AudioClip[] Sounds { get => sounds; }
+	[HideInInspector] public string name;
+	[SerializeField] private AudioClip[] sounds;
 }
